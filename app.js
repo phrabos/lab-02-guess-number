b8388e00dccda2564997b89b579e7dc427f7ae1a
Patrick Hrabos <phrabos@gmail.com>
2021-01-12 16:56:39 -0500

    create and test function to compare guess vs. actual number

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 // import functions and grab DOM elements
+import { compareNumbers } from "./utils.js";
 const guessesRemainingSpan = document.getElementById("remaining-guesses");
 const resultsParagraph = document.getElementById("results");
+const resultsSpan = document.getElementById("results-span")
 const winLoseParagraph = document.getElementById("win-lose");
 const winLoseSpan = document.getElementById("win-lose-span");
-const numberGuess = document.getElementById("number-guess");
+const numberGuessInput = document.getElementById("number-guess");
 const submitButton = document.getElementById("submit-button");
 const resetButton = document.getElementById("reset-button");
 
+const someTest = compareNumbers(1,3);
+console.log(someTest)
 
 // initialize state
 let guessesRamaining = 4;
 let secretNumber = Math.ceil(Math.random() * 20)
+console.log(`this is the secret number ${secretNumber}`)
 
 // set event listeners to update state and DOM
 submitButton.addEventListener("click", () => {
-    console.log("button clicked")
+    --guessesRamaining
+    guessesRemainingSpan.textContent = guessesRamaining;
+    resultsParagraph.classList.remove("hidden");
+    // numberGuessInput.value = '';
+    let numberGuess = numberGuessInput.value;
+    if(numberGuess > secretNumber){
+        resultsSpan.textContent = "too high";
+    } else if (numberGuess < secretNumber) {
+        resultsSpan.textContent = "too low"
+    } else {
+        winLoseParagraph.classList.remove("hidden");
+        resultsParagraph.classList.add("hidden");
+        winLoseSpan.textContent = "Congratulations, you win!!!"
+    }
+  console.log(numberGuess)  
 })
 
 resetButton.addEventListener("click", () => {
-    console.log("button clicked")
+    
 })
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,45 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { compareNumbers } from "../utils.js";
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('if guess is 7 & correctNumber 3', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 1;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = compareNumbers(7,3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('if guess is 1 & correctNumber 3', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = -1;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(1,3);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+test('if guess is 3 & correctNumber 3', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 0;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(3,3);
 
     //Expect
     // Make assertions about what is expected versus the actual result
